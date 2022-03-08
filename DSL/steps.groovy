a096ed54140637f1ddb822957bc8260a4df8c361
apgrados <101198969+apgrados@users.noreply.github.com>
2022-03-08 18:47:07 +0100

    Update steps.groovy

--- DSL/steps.groovy
+++ DSL/steps.groovy
@@ -1,7 +1,7 @@
 job('javaappDSL') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/Adastra-THW/jenkinsmaven.git', 'master') { node ->
+        git('https://github.com/apgrados/jenkinsmaven.git', 'master') { node ->
             node / gitConfigName('adastra')
             node / gitConfigEmail('adastra@thehackerway.com')
         }
